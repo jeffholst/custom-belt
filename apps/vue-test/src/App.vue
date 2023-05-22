e4514da4b1a2a03331ce3e00af43cff4a4ed5fcd
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-05-21 22:07:17 -0500

    chore: added vue-test

---
+++ apps/vue-test/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <CustomBelt :belt-props="whiteBelt" />
+</template>
+
+<script setup lang="ts">
+import CustomBelt from "../../../packages/vue-custom-belt/src/components/CustomBelt.vue"
+import { getBeltPropsSolid } from "../../../packages/custom-belt-lib/src/Belt"
+
+const whiteBelt = getBeltPropsSolid("Solid White Belt", "#00FF00")
+</script>
+
+<style scoped></style>
